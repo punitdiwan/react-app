7944dea6c352562fcdb79aad0c69435a8622337a
Punit Diwan <punitdiwan@gmail.com>
2019-07-03 12:00:28 -0700

    Events in React Components

--- src/App.js
+++ src/App.js
@@ -2,24 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  state = {
+    input :'',
+    reversed:'',
+    data:''
+  }
+
+  changeHandler = (e) => {
+    const inputTxt = e.target.value;
+    this.setState({input: inputTxt, data: inputTxt});
+  }
+
+  clickHandler = (e)=>{
+    const txt = this.state.input;
+    let array = txt.split('').reverse();
+    let reversedString =  array.join('');
+    this.setState({data: reversedString, input:''});
+    
+  }
+
+ render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-
-        >
-          Learn React
-        </a>
+       <label>Text: {this.state.data}</label>
+       <input type="text"
+       value={this.state.input}
+       placeholder="Enter Text"
+       onChange={this.changeHandler}
+       />
+       <button type="button" value="reverse"
+       onClick={this.clickHandler} >Reverse</button>
       </header>
     </div>
   );
+ }
 }
 
 export default App;
